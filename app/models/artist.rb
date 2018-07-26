61a37bb0ce668384ade1a9368a9ad42464cf200e
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-26 00:39:30 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,7 +13,7 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    self.songs.genre.uniq.count
+    self.songs.genres.uniq.count
     #return the number of genres associated with the artist
   end
 end
