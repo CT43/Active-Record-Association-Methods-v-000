28650e35fefd9e85239b557f1c6fd7fd3ad4e3ce
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-26 00:42:52 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists.each.name
+    self.artists.collect.name
     # return an array of strings containing every musician's name
   end
 end
